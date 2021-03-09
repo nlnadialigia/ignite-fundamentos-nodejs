368e4c72a6ecd2d34922f5ce53cf863a82f8244a
Nadia <nlnadialigia@gmail.com>
2021-03-09 00:01:38 -0300

    feat: add example for all HTTP methods

--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World Ignite!' })
+app.get('/courses', (request, response) => {
+  return response.json(['Curso 1', 'Curso 2', 'Curso 3'])
+});
+
+app.post('/courses', (request, response) => {
+  return response.json(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4'])
+})
+
+app.put('/courses/:id', (request, response) => {
+  return response.json(['Curso 6', 'Curso 2', 'Curso 3', 'Curso 4'])
+})
+
+app.patch('/courses/:id', (request, response) => {
+  return response.json(['Curso 6', 'Curso 7', 'Curso 3', 'Curso 4'])
+})
+
+app.delete('/courses/:id', (request, response) => {
+  return response.json(['Curso 6', 'Curso 7', 'Curso 4'])
 })
 
 app.listen(3333);
