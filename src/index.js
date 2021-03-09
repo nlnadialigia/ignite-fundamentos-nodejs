41e4fdacc7df1919effd10c7168f923c5ce91810
Nadia <nlnadialigia@gmail.com>
2021-03-09 00:33:33 -0300

    refactor: refactor eslint config

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
 app.get('/courses', (request, response) => {
-  const query = request.query
-  console.log(query)
-  return response.json(['Curso 1', 'Curso 2', 'Curso 3'])
-})
+  const query = request.query;
+  console.log(query);
+  return response.json(['Curso 1', 'Curso 2', 'Curso 3']);
+});
 
 app.post('/courses', (request, response) => {
-  const body = request.body
-  console.log(body)
-  return response.json(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4'])
-})
+  const body = request.body;
+  console.log(body);
+  return response.json(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4']);
+});
 
 app.put('/courses/:id', (request, response) => {
-  const params = request.params
-  console.log(params)
-  return response.json(['Curso 6', 'Curso 2', 'Curso 3', 'Curso 4'])
-})
+  const params = request.params;
+  console.log(params);
+  return response.json(['Curso 6', 'Curso 2', 'Curso 3', 'Curso 4']);
+});
 
 app.patch('/courses/:id', (request, response) => {
-  return response.json(['Curso 6', 'Curso 7', 'Curso 3', 'Curso 4'])
-})
+  return response.json(['Curso 6', 'Curso 7', 'Curso 3', 'Curso 4']);
+});
 
 app.delete('/courses/:id', (request, response) => {
-  return response.json(['Curso 6', 'Curso 7', 'Curso 4'])
-})
+  return response.json(['Curso 6', 'Curso 7', 'Curso 4']);
+});
 
-app.listen(3333)
+app.listen(3333);
