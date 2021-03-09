24b37549af890351f178955a448041271bf363f6
Nadia <nlnadialigia@gmail.com>
2021-03-09 00:22:54 -0300

    feat: add example for all Type of Params

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-import express from 'express';
+import express from 'express'
 
-const app = express();
+const app = express()
 
 app.get('/courses', (request, response) => {
+  const query = request.query
+  console.log(query)
   return response.json(['Curso 1', 'Curso 2', 'Curso 3'])
-});
+})
 
 app.post('/courses', (request, response) => {
+  const body = request.body
+  console.log(body)
   return response.json(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4'])
 })
 
 app.put('/courses/:id', (request, response) => {
+  const params = request.params
+  console.log(params)
   return response.json(['Curso 6', 'Curso 2', 'Curso 3', 'Curso 4'])
 })
 
@@ -22,4 +28,4 @@ app.delete('/courses/:id', (request, response) => {
   return response.json(['Curso 6', 'Curso 7', 'Curso 4'])
 })
 
-app.listen(3333);
+app.listen(3333)
